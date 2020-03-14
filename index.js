ef6d0b88596e53b067a304448536b92f2a3a0ac5
Nicholas Jaquez <nrjaquez@gmail.com>
2020-03-14 13:31:03 -0700

    nick's push

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 
+
 //PORT
 const app = express();
 const PORT = 5000;
@@ -10,40 +11,84 @@ app.use(express.json());
 
 //DATA
 
-let tables = [
-  {
-    tableNumber: "1"
-  },
-  {
-    tableNumber: "2"
-  },
-  {
-    tableNumber: "3"
-  }
+let waitlist = []
+
+let tables =
+[
+    {
+        tableNumber: "1"
+    },
+    {
+        tableNumber: "2"
+    },
+    {
+        tableNumber: "3"
+    }
 ];
+
 // Routes to html
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/tables", function(req, res) {
-  res.sendFile(path.join(__dirname, "tables.html"));
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
 });
 
-app.get("/input", function(req, res) {
-  res.sendFile(path.join(__dirname, "input.html"));
+app.get("/input", function (req, res) {
+    res.sendFile(path.join(__dirname, "input.html"));
 });
 
-app.get("/api/tables:table", function(req, res) {
-  let chosen = req.params.table;
+app.get("/api/tables", function (req, res) {
+    let chosen = req.params.table;
 
-  console.log(chosen);
+    console.log(chosen);
+
+
+
+    //   for (let index = 0; index < tables.length; index++) {
+    //     if (chosen === tables[i].tableNumber){
+    //         return res.json (tables[i]);
+    //     }
+    //     return res.json(false);
+    // }
+
 });
 
-//GET tables avail and waitlist
+app.get("/api/waitlist", function (req, res) {
+    res.json(
+        {
+            firstName: "Steven",
+            customerEmail: "njaquez@yahoo.com",
+            cusomterID: "101",
+            phoneUnumer: "3239799318",
+        })
+
+})
+
+app.post("/api/tables", function(req, res){
+
+    
+    const newList = req.body
+    if (tables.length < 5){
+        tables.push(newList)
+        res.json("you're booked")
+    } else {
+        waitlist.push(newList);
+        res.json("You're on the waitlist")
+    } 
+
+    
+
+    res.json("");
+})
+
+
+
+
 
 //POST new people on list
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
 });
