1948cfa4c24ac4e2d6325d24a378f39912e32490
Anna Olt <anna.olt16@gmail.com>
2020-03-14 15:15:44 -0400

    server branch

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-//dependencies, express?
+const express = require("express");
+const path = require("path");
 
 //PORT
+const app = express();
+const PORT = 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //DATA
 
-//Route to get to tables page
+let tables = [
+  {
+    tableNumber: "1"
+  },
+  {
+    tableNumber: "2"
+  },
+  {
+    tableNumber: "3"
+  }
+];
+// Routes to html
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-//Route to get to form page
+//Route to API
 
 //GET tables avail and waitlist
 
 //POST new people on list
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
